aba33caff75583fa05c84f1e49f8ac6815c7eddc
Ayesha <pu_ash@yahoo.com>
2023-05-16 22:50:08 +0100

    footer added and styled.

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-function validateForm () {
-    var name = document.getelementbyid("name").value;
-    var name = document.getelementbyid("email").value;
-    var name = document.getelementbyid("message").value;
-    var error = " ";
 
-    if (name == " ") {
+function validateForm(event) {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    let error = " ";
+    
+    if (name === "") {
+        console.log('i m empty')
         error += "Please enter Your name. \n";
     }
     
-    if (email == " ") {
+    if (email === "") {
         error += "Please enter Your Email. \n";
-    } else if (!validateEmail (email)) {
+    } else if (!validateEmail(email)) {
         error += "Please enter a valid email address.\n"
     }
    
-
-    if (message == " ") {
+    if (message === "") {
         error += "Please enter a message. \n";
     }
 
-    if (error != " ") {
+    if (error !== " ") {
         alert(error);
         return false;
     }
 }
 
-function validateEmail (email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+function validateEmail(email) {
+    const regPattern = /\S+@\S+\.\S+/;
+    return regPattern.test(email);
 }
+
